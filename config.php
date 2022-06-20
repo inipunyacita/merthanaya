11182a2a43c127a23d9d88fd8889c9cf0dfb0bb1
inipunyacita <1915323089@pnb.ac.id>
2022-06-20 21:37:39 +0800

    change config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "db_merthanaya";
+$db_user = "citanant_admin";
+$db_pass = "citaadmin12345";
+$db_name = "citanant_merthanaya";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
