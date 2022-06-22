41eed87643cd404cdc48846ebc0e9e87b41a46b7
inipunyacita <1915323089@pnb.ac.id>
2022-06-22 09:26:59 +0800

    add pagination dashboard

--- adminpage.php
+++ adminpage.php
@@ -82,14 +82,25 @@ $data_user = $conn->query($sql);
                     </thead>
                     <tbody>
                         <?php
+                        $batas = 10;
+                        $halaman = isset($_GET['halaman']) ? (int)$_GET['halaman'] : 1;
+                        $halaman_awal = ($halaman > 1) ? ($halaman * $batas) - $batas : 0;
+
+                        $previous = $halaman - 1;
+                        $next = $halaman + 1;
+
+                        $data = $conn->query("SELECT * FROM user");
+                        $jumlah_data = mysqli_num_rows($data);
+                        $total_halaman = ceil($jumlah_data / $batas);
                         if (isset($_GET['cari']) && $_GET['cari'] != null) {
                             $cari = $_GET['cari'];
-                            $sql = "SELECT * FROM `user` WHERE (`id`like'%$cari%') or (`username`like'%$cari%')";
+                            $sql = "SELECT * FROM `user` WHERE (`id`like'%$cari%') or (`username`like'%$cari%') limit $halaman_awal, $batas";
                             $data_user = $conn->query($sql);
                         } else {
-                            $sql_user = "SELECT * FROM user";
+                            $sql_user = "SELECT * FROM user limit $halaman_awal, $batas";
                             $data_user = $conn->query($sql_user);
                         }
+                        $nomor = $halaman_awal + 1;
                         $no = 1;
 
                         while ($data = mysqli_fetch_assoc($data_user)) { ?>
@@ -159,6 +170,24 @@ $data_user = $conn->query($sql);
                     </tbody>
                 </table>
             </div>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item">
+                        <a class="page-link" <?php if ($halaman > 1) {
+                                                    echo "href='?halaman=$previous'";
+                                                } ?>>Previous</a>
+                    </li>
+                    <?php
+                    for ($x = 1; $x <= $total_halaman; $x++) {
+                    ?>
+                        <li class="page-item"><a class="page-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
+                    <?php } ?>
+                    <li class="page-item"><a class="page-link" <?php if ($halaman < $total_halaman) {
+                                                                    echo "href='?halaman=$next'";
+                                                                } ?>>Next</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </div>
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
