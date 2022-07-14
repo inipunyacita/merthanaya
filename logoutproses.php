ae89e8077a8d90a23a36548681367042c733ae46
inipunyacita <1915323089@pnb.ac.id>
2022-07-14 10:19:59 +0800

    update session logout #2

--- logoutproses.php
+++ logoutproses.php
@@ -6,6 +6,7 @@ if (isset($_SESSION['userloggedin'])) {
     header('Location: index.php');
     exit;
 } elseif (isset($_SESSION["superadminloggedin"])) {
+    unset($_SESSION['superadminloggedin']);
     header('Location: index.php');
     exit;
 } else {
