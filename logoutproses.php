abdc57763501e252c36edd210bc307c4afa048a7
inipunyacita <1915323089@pnb.ac.id>
2022-07-14 10:18:05 +0800

    change logout proses

--- logoutproses.php
+++ logoutproses.php
@@ -5,6 +5,9 @@ if (isset($_SESSION['userloggedin'])) {
     unset($_SESSION['userloggedin']);
     header('Location: index.php');
     exit;
+} elseif (isset($_SESSION["superadminloggedin"])) {
+    header('Location: index.php');
+    exit;
 } else {
     unset($_SESSION['adminloggedin']);
     header('Location: index.php');
