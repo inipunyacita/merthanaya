7136150f1705176959bb01f69a8f03ee46f5b7a1
inipunyacita <inipunyacita@gmail.com>
2022-11-04 15:05:20 +0800

    update connection

--- event.php
+++ event.php
@@ -1,3 +1,32 @@
+<?php
+
+include_once("config.php");
+
+session_start();
+
+$sql = "SELECT * FROM user";
+$data_user = $conn->query($sql);
+
+if (isset($_SESSION['userloggedin'])) {
+  echo "<script>
+        alert('Silahkan log out dari user terlebih dahulu!');
+        window.location.href = 'userpage.php';
+    </script>";
+}
+if (isset($_SESSION['adminloggedin'])) {
+  echo "<script>
+        alert('Silahkan log out dari admin terlebih dahulu!');
+        window.location.href = 'adminpage.php';
+    </script>";
+}
+if (isset($_SESSION['superadminloggedin'])) {
+  echo "<script>
+        alert('Silahkan log out dari super admin terlebih dahulu!');
+        window.location.href = 'adminpage.php';
+    </script>";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
